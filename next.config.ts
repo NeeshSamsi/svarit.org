e79bb630ebb2aa7a17661b50abde37d8be67f315
Neesh <avaneeshsamsi@gmail.com>
2025-02-23 18:44:28 +0100

    feat: Redirects for DS 25 support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: "/DS25",
+        destination: "https://forms.gle/5ymhiMrYK4S2dp8AA",
+        permanent: true,
+      },
+      {
+        source: "/ds25",
+        destination: "https://forms.gle/5ymhiMrYK4S2dp8AA",
+        permanent: true,
+      },
+      {
+        source: "*",
+        destination: "https://instagram.com/svaritorg",
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
