581544f41ef8a5bd6e39548f83e043c7089ba641
Neesh <avaneeshsamsi@gmail.com>
2026-02-21 15:59:33 +0700

    chore: disable UKS Google Form redirect

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
     return [
-      {
-        source: "/UKS",
-        destination: "https://forms.gle/9Nz9JoopR4j7soLe8",
-        permanent: true,
-      },
-      {
-        source: "/uks",
-        destination: "https://forms.gle/9Nz9JoopR4j7soLe8",
-        permanent: true,
-      },
+      // {
+      //   source: "/uks",
+      //   destination: "https://forms.gle/9Nz9JoopR4j7soLe8",
+      //   permanent: true,
+      // },
       {
         source: "/:path*",
         destination: "https://instagram.com/svaritorg",
