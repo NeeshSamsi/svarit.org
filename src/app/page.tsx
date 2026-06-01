13601ef1e877d882bfe050931a53fbf6537974e7
Neesh <avaneeshsamsi@gmail.com>
2026-06-01 23:51:49 +0700

    feat: home page composition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Hero from "@/components/home/Hero";
-import About from "@/components/home/About";
-import Initiatives from "@/components/home/Initiatives";
-import Contact from "@/components/home/Contact";
+import Hero from '@/components/home/Hero'
+import About from '@/components/home/About'
+import Initiatives from '@/components/home/Initiatives'
+import Donate from '@/components/home/Donate'
+import Contact from '@/components/home/Contact'
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
       <Hero />
       <About />
       <Initiatives />
+      <Donate />
       <Contact />
     </>
-  );
+  )
 }
